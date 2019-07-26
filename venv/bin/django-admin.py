40f4e1b14f3547ce99f1225582f7e4521c5ca907
santosh <khadkasantosh009>
2019-07-26 20:02:31 +0545

    models created

---
+++ venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/media/santosh/KHADKAS/Python/django_project/video_subscription_app/venv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
